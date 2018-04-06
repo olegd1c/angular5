4b070d5854def3b476f1acf6179105cd62c171eb
sergey <sergey.morenets@yandex.ua>
2018-04-06 14:20:34 +0300

    Task #5. Generate latin characters

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {Book} from '../book/book';
 import {BookService} from '../book.service';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/range';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-books',
@@ -13,6 +16,9 @@ export class BooksComponent {
   books: Array<Book>;
 
   constructor(private bookService: BookService) {
+    Observable.range(0, 25).map(x => String.fromCharCode(x + 'a'.charCodeAt(0)))
+      .subscribe(x => console.log(x));
+
     this.refreshBooks();
   }
 
