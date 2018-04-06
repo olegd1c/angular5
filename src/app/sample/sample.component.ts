cbfdf03fbb1a03dd4490035bbc9951215564fd66
sergey <sergey.morenets@yandex.ua>
2018-04-06 11:46:20 +0300

    Task #3. Example 6

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,4 +1,5 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import {Renderer3} from '@angular/core/src/render3/renderer';
 
 @Component({
   selector: 'app-sample',
@@ -7,12 +8,12 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class SampleComponent implements OnInit {
 
-  label: string;
-  //
-  // @ViewChild('text')
-  // myLabel: ElementRef;
+  // label: string;
 
-  constructor() {
+  @ViewChild('text')
+  myLabel: ElementRef;
+
+  constructor(private renderer: Renderer2) {
   }
 
   ngOnInit() {
@@ -20,7 +21,9 @@ export class SampleComponent implements OnInit {
 
   changeLabel(elem: ElementRef) {
     // this.myLabel.nativeElement.innerHTML = 'New text3';
-    //this.label = 'New <b>text4</b>';
-    elem.nativeElement.innerHTML = 'New Text5';
+    // this.label = 'New <b>text4</b>';
+    // elem.nativeElement.innerHTML = 'New Text5';
+    this.renderer.setProperty(this.myLabel.nativeElement,
+      'innerHTML', 'New text6');
   }
 }
